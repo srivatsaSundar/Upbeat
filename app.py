13a1198b2806fabd6d035601585153256cfcebf1
srivatsaSundar <srivatsas0503@gmail.com>
2024-03-25 10:12:43 +0530

    yml file updated

--- app.py
+++ app.py
@@ -10,5 +10,6 @@ app.include_router(user_router,prefix='/user',tags=['users'])
 app.include_router(profile_router,prefix='/profile',tags=['profile'])
 Base.metadata.create_all(bind=engine)
 
+
 if __name__=="__main__":
     uvicorn.run('app:app',host='0.0.0.0',port=8080,reload=True)
